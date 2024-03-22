f1a91d4c76ffa480e88bd12041bdd159c573a588
Saipavanvadapally <pavanvadapally034@gmail.com>
2024-03-22 21:09:52 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import "./App.css";
+
+import KabanBoard from './components/KabanBoard';
+ 
+function App() {
+  return(
+    <KabanBoard/>
+    
+ 
+  );
+     
+}
+
+export default App
---
+++ src/components/KabanBoard.tsx
@@ -0,0 +1,68 @@
+ 
+import Plusicon from "../icons/Plusicon";
+import { useState } from "react";
+import { Column } from "../types";
+import ColumnContainer from "./ColumnContainer";
+function KabanBoard() {
+    const [columns, setColumns]=useState<Column[]>([]);
+    console.log(columns);
+  return (
+    <div className="
+    m-auto
+    flex
+    min-h-screen
+    w-full
+    items-center
+    overflow-x-auto
+    overflow-y-hidden
+    px-[40px]
+    "
+    >
+        < div className='m-auto flex gap-4'>
+            <div className="flex gap-4">
+                {columns.map((col)=>(
+                    <ColumnContainer column={col}/>
+
+                ))}
+                </div>  
+        <button
+        onClick={()=>{
+            createNewColumn();
+        }}
+        className=" 
+        h-[60px]
+        w-[350px]
+        min-w-[350px]
+        cursor-pointer
+        rounded-lg
+        bg-mainBackgroundColor
+        border-2
+        border-columnBackgroundColor
+        p-4
+        ring-rose-500
+        hover:ring-2
+        flex
+        gap-2
+        "
+        >
+            <Plusicon/>
+            Add Column
+         </button>
+        </div>
+    </div>
+  );
+  function createNewColumn(){
+    const columnToAdd:Column={
+        id:generateId(),
+        title:`Column ${columns.length+1}`,
+    };
+    setColumns([...columns,columnToAdd]);
+
+  }
+}
+function generateId(){
+    return Math.floor(Math.random()*10001);
+
+}
+
+export default KabanBoard;
